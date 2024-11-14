38a526a4f9ceaae210fa619a354f9b0576036e5d
florian <florianbarjon@icloud.com>
2024-11-14 16:51:59 +0100

    First commit

---
+++ src/pages/About.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, vitae
+        error asperiores maxime dolore neque molestiae, nulla quia culpa officia
+        sit alias ab facere dignissimos modi ipsam praesentium voluptate minima
+        aliquam quo veniam quod rem doloremque? Ipsam optio odio accusamus quia
+        alias fuga vero dolorem, ratione distinctio minus deserunt praesentium.
+        At deleniti ducimus sit officiis. Earum possimus, rem sunt enim sequi
+        fuga, dolorem id, alias libero dolor quisquam quo commodi corrupti
+        neque? Rerum vel ducimus laudantium dolore debitis qui nesciunt illum
+        tempore reprehenderit iure similique blanditiis nulla asperiores iusto
+        repudiandae corporis odit, eveniet obcaecati tenetur facilis hic tempora
+        esse optio!
+      </p>
+      <br />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi sit
+        dolor, quibusdam cum incidunt qui officia voluptates a illo in adipisci
+        neque id, ea distinctio delectus earum ab eaque dolorem laborum? Officia
+        impedit quo consequuntur iste, dolorem ad adipisci nemo inventore fuga
+        provident velit libero iure temporibus aliquid voluptatum eum asperiores
+        dolor vel minima dolore qui est ratione! Cumque ipsum atque, similique
+        pariatur fugit quos porro voluptatem facere animi enim exercitationem
+        error hic eveniet non consequuntur excepturi dolor quam illo assumenda
+        consectetur? Quisquam quos recusandae laborum dolorem maiores a tenetur
+        repudiandae iusto nihil nostrum labore quidem fugit obcaecati,
+        architecto rerum exercitationem nesciunt corrupti ullam in maxime dicta
+        odio libero eligendi aliquid? Dolor nesciunt adipisci consequatur eum
+        error totam iste vero dolorum, assumenda tempora ducimus sequi tenetur
+        doloremque nulla fugiat est cupiditate iure. Aperiam similique sapiente
+        ipsam harum quas natus nulla odio doloribus vitae fugiat molestiae
+        mollitia, quis nesciunt deleniti, voluptate voluptates! Ex, in quaerat
+        fugit libero accusantium amet eum ipsam quis enim consequatur architecto
+        atque iste recusandae ducimus officia, corrupti magni esse sapiente id
+        inventore, eius ad impedit temporibus eveniet? Eos error aut, laboriosam
+        voluptatibus beatae quae obcaecati officiis! Tempora quia quae dicta
+        expedita dolores a aliquid et harum hic.
+      </p>
+    </div>
+  );
+};
+
+export default About;
